d71be3aa66f81c0df59638f1b5a6187445400932
Mfon Udo <mfonprog@gmail.com>
2022-12-11 13:00:21 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
+
+
 /**
-*Usint puts function to Prints the string to the console 
-"Programming is like building a multilingual puzzle,followed by a new line".
+*main-Prints"Programming is like building a multilingual
+*       puzzle,followed by a new line".
 *
 *Return:Always 0.
 **/
+
+
 int main(void)
+
+
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
