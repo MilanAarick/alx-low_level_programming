d8025fd58843bf38f6782bf060567cfd7588fa2a
MilanAarick <mfonprog@gmail.com>
2022-12-12 06:34:39 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
 /**
- * Write a program that prints the alphabet in lowercase, 
- * followed by a new line.
- *
- * Return: Always 0 (Success)
- */
-
-
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0.
+**/
 int main(void)
-
-
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
