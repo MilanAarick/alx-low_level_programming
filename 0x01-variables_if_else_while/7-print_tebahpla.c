363cafb11a4d58c7b561a9f8fc6bab806f814e1b
MilanAarick <mfonprog@gmail.com>
2022-12-12 07:05:32 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,25 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
 /**
- * Write a program that prints the lowercase alphabet in reverse,
- *  followed by a new line.
+ * main - Write a program that prints the lowercase alphabet in reverse,
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
-
-
 {
-	char low;
-
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n');
+return (0);
 }
