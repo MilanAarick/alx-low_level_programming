db6d737f6451fdf72c22b1d3ddd8459d9fba3af4
MilanAarick <mfonprog@gmail.com>
2022-12-12 06:48:45 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints the alphabet in lowercase, followed by a new line.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints the alphabet in lowercase, except for q and e.
+*
+* Return: Always 0.
+**/
 int main(void)
-
-
 {
-	char low, e, q;
-
-	e = 'e';
-	q = 'q';
-
-	for (low = 'a'; low <= 'z'; low++)
-	{
-		if (low != e && low != q)
-			putchar(low);
-	}
-	putchar('\n');
-
-	return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'e' && letter != 'q')
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
