e2e92a7a2a9047b18aa68ded8ea2be676dafa594
MilanAarick <mfonprog@gmail.com>
2022-12-12 06:45:43 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - Write a program that prints the alphabet in lowercase,
+ * main - Write a program that prints the alphabet in lowercase,
   followed by a new line.
  *
  * Return: Always 0 (Success)
