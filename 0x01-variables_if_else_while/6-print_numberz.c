fba263dffe9269bb9c4d9ec2d7d8d4ec389715ac
MilanAarick <mfonprog@gmail.com>
2022-12-12 07:03:00 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints all single digit numbers of base 10 starting from 0,
+*        only using putchar and without char variables.
+*
+* Return: Always 0.
+**/
 int main(void)
 {
-int d;
-
-for (d = '0'; d <= '9'; d++)
-    putchar(d);
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
 putchar('\n');
-
 return (0);
 }
