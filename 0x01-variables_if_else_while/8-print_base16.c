743ffe2f68ec925e39cbc904ab2e1343d2338f9e
MilanAarick <mfonprog@gmail.com>
2022-12-12 07:08:56 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints all the numbers of base 
- * 16 in lowercase, followed by a new line.
+ * main - Write a program that prints all the numbers of base 16 in lowercase, followed by a new line.
  *
  * Return: Always 0 (Success)
  */
