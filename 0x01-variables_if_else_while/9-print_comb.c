bafb147c7cc194b7d107f2e4549aed6a4cc7e16f
MilanAarick <mfonprog@gmail.com>
2022-12-12 07:12:39 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
- * Write a program that prints all possible
- *  combinations of single-digit numbers.
+ * main - Write a program that prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int d;
