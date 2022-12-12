7d9569d3dfd314bc262a5e4f173353d1fe8ce69d
MilanAarick <mfonprog@gmail.com>
2022-12-12 06:56:48 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints all single digit numbers of base 10 starting from 0, 
- * followed by a new line.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints all single digit numbers of base 10 starting from 0.
+*
+* Return: Always 0.
+**/
 int main(void)
-
-
 {
-	int i;
-
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
-	printf("\n");
-
-	return (0);
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
 }
