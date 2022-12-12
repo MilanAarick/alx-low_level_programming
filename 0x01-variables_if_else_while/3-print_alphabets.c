03f66c52d1ea4387a0ad19521c7263c0e57d728a
MilanAarick <mfonprog@gmail.com>
2022-12-12 06:40:45 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * main - Write a program that prints the alphabet in lowercase, and then in uppercase, 
- * followed by a new line.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints the alphabet in lowercase, and then in uppercase.
+*
+* Return: Always 0.
+**/
 int main(void)
-
 {
-	char low;
-
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-	putchar('\n');
-
-	return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
+putchar('\n');
+return (0);
 }
