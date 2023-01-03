95f0c8fc1a5bd9be983daf0ec82e8190a02384f5
MilanAarick <mfonprog@gmail.com>
2023-01-03 12:58:41 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,6 +7,7 @@
  * @b: The character to fill the memory area with.
  * @n: The number of bytes to be filled.
  *
+ * 
  * Return: A pointer to the filled memory area @s.
  */
 char *_memset(char *s, char b, unsigned int n)
