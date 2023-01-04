76ca7350df41743b9815e6745bb62a4a41a19792
MilanAarick <mfonprog@gmail.com>
2023-01-04 17:30:34 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,20 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * _puts_recursion - Recursive function that prints a string followed by a new line
  * @s: String to print
  */
+
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        putchar('\n');
-        return;
-    }
-    putchar(*s);
-    _puts_recursion(s + 1);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
