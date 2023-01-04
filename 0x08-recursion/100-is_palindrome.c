d519ec25850dc9d2e19175680dc8621ec840c715
MilanAarick <mfonprog@gmail.com>
2023-01-04 18:09:21 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * _strlen_recursion -  
- * a function that returns 1 if a string is a palindrome and 0 if not.
+ * _strlen_recursion -  a function that returns 1 
+ * if a string is a palindrome and 0 if not.
  * @s: pointer to string params
  * Return: recursion
  */
@@ -19,6 +18,8 @@ int _strlen_recursion(char *s)
 
 /**
  * p1 - palindrome
+ * @s: pointer to string
+ * @l: position
  * Return: boolena
  */
 
